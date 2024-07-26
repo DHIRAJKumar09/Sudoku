a8116c759bbcd14a61b1213252b0d15c63b045fc
DHIRAJKumar09 <dhirajsinghania09@gmail.com>
2024-07-27 03:35:16 +0530

    solved code

--- src/Components/Sudookuu/Sudookuu.js
+++ src/Components/Sudookuu/Sudookuu.js
@@ -1,100 +1,88 @@
 import React, { useState } from 'react';
 import './Sudookuu.css';
 
-const Sudookuu = () => {
-  const initial = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-  ];
+const initial = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
 
+const getDeepCopy = (arr) => {
+  return JSON.parse(JSON.stringify(arr));
+};
+
+const isSafe = (grid, row, col, num) => {
+  for (let x = 0; x < 9; x++) {
+    if (grid[row][x] === num || grid[x][col] === num) return false;
+    const startRow = 3 * Math.floor(row / 3);
+    const startCol = 3 * Math.floor(col / 3);
+    if (grid[startRow + Math.floor(x / 3)][startCol + (x % 3)] === num) return false;
+  }
+  return true;
+};
+
+const findEmptyLocation = (grid, loc) => {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (grid[row][col] === 0) {
+        loc[0] = row;
+        loc[1] = col;
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const solver = (grid) => {
+  const loc = [0, 0];
+  if (!findEmptyLocation(grid, loc)) return true;
+
+  const row = loc[0];
+  const col = loc[1];
+
+  for (let num = 1; num <= 9; num++) {
+    if (isSafe(grid, row, col, num)) {
+      grid[row][col] = num;
+      if (solver(grid)) return true;
+      grid[row][col] = 0; // Backtrack
+    }
+  }
+  return false;
+};
+
+const Sudookuu = () => {
   const [arr, setArr] = useState(getDeepCopy(initial));
 
-  function getDeepCopy(arr) {
-    return JSON.parse(JSON.stringify(arr));
-  }
-
-  function onInputChange(e, row, col) {
+  const onInputChange = (e, row, col) => {
     const val = parseInt(e.target.value) || 0;
     const grid = getDeepCopy(arr);
 
     if (val >= 1 && val <= 9) {
       grid[row][col] = val;
-      if (!checkValid(grid, row, col, val)) {
+      if (!isSafe(grid, row, col, val)) {
         alert("Invalid move! This number causes a duplicate in the row, column, or 3x3 grid.");
-      } else {
-        setArr(grid);
       }
+      setArr(grid);
     } else {
       grid[row][col] = 0;
       setArr(grid);
     }
-  }
+  };
 
-  function checkRow(grid, row, num) {
-    return grid[row].filter(x => x === num).length <= 1;
-  }
-
-  function checkCol(grid, col, num) {
-    return grid.map(row => row[col]).filter(x => x === num).length <= 1;
-  }
-
-  function checkBox(grid, row, col, num) {
-    const boxArr = [];
-    const rowStart = row - (row % 3);
-    const colStart = col - (col % 3);
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        boxArr.push(grid[rowStart + i][colStart + j]);
-      }
-    }
-    return boxArr.filter(x => x === num).length <= 1;
-  }
-
-  function checkValid(grid, row, col, num) {
-    return (
-      checkRow(grid, row, num) &&
-      checkCol(grid, col, num) &&
-      checkBox(grid, row, col, num)
-    );
-  }
-
-  function getNext(row, col) {
-    return col !== 8 ? [row, col + 1] : [row + 1, 0];
-  }
-
-  function solver(grid, row = 0, col = 0) {
-    if (row === 9) return true; // Puzzle solved
-    if (grid[row][col] !== 0) {
-      const [newRow, newCol] = getNext(row, col);
-      return solver(grid, newRow, newCol);
-    }
-    for (let num = 1;  num <= 9; num++) {
-      if (checkValid(grid, row, col, num)) {
-        grid[row][col] = num;
-        const [newRow, newCol] = getNext(row, col);
-        if (solver(grid, newRow, newCol)) {
-          return true;
-        }
-        grid[row][col] = 0; // Backtrack
-      }
-    }
-    return false;
-  }
-
-  function solveSudoku() {
+  const solveSudoku = () => {
     const sudoku = getDeepCopy(arr);
     solver(sudoku);
     setArr(sudoku);
-  }
+  };
 
-  function compareSudokus(currentSudoku, solvedSudoku) {
+  const compareSudokus = (currentSudoku, solvedSudoku) => {
     let res = {
       isComplete: true,
       isSolved: true,
@@ -110,9 +98,9 @@ const Sudookuu = () => {
       }
     }
     return res;
-  }
+  };
 
-  function checkSudoku() {
+  const checkSudoku = () => {
     let sudoku = getDeepCopy(initial);
     solver(sudoku);
     let compare = compareSudokus(arr, sudoku);
@@ -124,7 +112,7 @@ const Sudookuu = () => {
     } else {
       alert("Sudoku can't be solved. Try again.");
     }
-  }
+  };
 
   return (
     <div className='App'>
@@ -160,6 +148,6 @@ const Sudookuu = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Sudookuu;
